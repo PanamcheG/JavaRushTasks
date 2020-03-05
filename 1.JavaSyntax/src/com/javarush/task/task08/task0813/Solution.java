75e1958f47d793725702d9d8c0876297ffe7e235
PanamcheG <yashkovich94@mail.ru>
2020-03-05 17:57:39 +0300

    05.03 changes

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0813/Solution.java
@@ -0,0 +1,42 @@
+package com.javarush.task.task08.task0813;
+
+import java.util.HashSet;
+import java.util.Set;
+
+/* 
+20 слов на букву «Л»
+*/
+
+public class Solution {
+    public static Set<String> createSet() {
+        //напишите тут ваш код
+        Set set = new HashSet();
+        set.add("Лень");
+        set.add("Лентяй");
+        set.add("Лосось");
+        set.add("Ласка");
+        set.add("Лопасть");
+        set.add("Лонгет");
+        set.add("Леприкон");
+        set.add("Ларнака");
+        set.add("Ласты");
+        set.add("Лесть");
+        set.add("Лед");
+        set.add("Луддит");
+        set.add("Лука");
+        set.add("Люцифер");
+        set.add("Ленин");
+        set.add("Лекало");
+        set.add("Лопата");
+        set.add("Лык");
+        set.add("Легкость");
+        set.add("Лещ");
+
+        return set;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
