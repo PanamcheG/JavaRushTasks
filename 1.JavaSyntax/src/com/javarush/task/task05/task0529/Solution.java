10417fa04bca62b9fa4eedec0d124dc83138cc6a
PanamcheG <yashkovich94@mail.ru>
2020-04-06 18:02:51 +0300

    06.04 tasks

--- 1.JavaSyntax/src/com/javarush/task/task05/task0529/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0529/Solution.java
@@ -3,9 +3,22 @@ package com.javarush.task.task05.task0529;
 /* 
 Консоль-копилка
 */
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 
 public class Solution {
     public static void main(String[] args) throws Exception {
-        //напишите тут ваш код
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+        int sum = 0;
+
+        while (true)
+        {
+            String s = buffer.readLine();
+            if (s.equals("сумма"))
+                break;
+            sum = sum + Integer.parseInt(s);
+        }
+
+        System.out.println(sum);
     }
 }
