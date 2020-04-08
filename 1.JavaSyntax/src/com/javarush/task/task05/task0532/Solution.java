4426ffa8533e8d289b08614015dc31fb546da59b
PanamcheG <yashkovich94@mail.ru>
2020-04-08 22:02:14 +0300

    08.04 tasks

--- 1.JavaSyntax/src/com/javarush/task/task05/task0532/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task05/task0532/Solution.java
@@ -10,10 +10,16 @@ import java.io.InputStreamReader;
 public class Solution {
     public static void main(String[] args) throws Exception {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
-        int maximum = ;
+        int N = Integer.parseInt(reader.readLine());
+        int maximum = Integer.MIN_VALUE;
 
-        //напишите тут ваш код
-
-        System.out.println(maximum);
+        if (N > 0) {
+            for (int i = 0; i < N; i++) {
+                int input = Integer.parseInt(reader.readLine());
+                if (maximum < input)
+                    maximum = input;
+            }
+            System.out.println(maximum);
+        }
     }
 }
