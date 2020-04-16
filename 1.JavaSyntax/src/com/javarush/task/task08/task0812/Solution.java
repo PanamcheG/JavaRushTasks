8d621bf92e36cb8a26e1ecd985a7604f79a18eb9
PanamcheG <yashkovich94@mail.ru>
2020-04-16 20:14:51 +0300

    16.04 tasks

--- 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0812/Solution.java
@@ -3,15 +3,32 @@ package com.javarush.task.task08.task0812;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 /* 
 Cамая длинная последовательность
 */
 public class Solution {
     public static void main(String[] args) throws IOException {
-        //напишите тут ваш код
+        List<Integer> list = new ArrayList<>();
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
 
+        for (int i = 0; i < 10; i++)
+            list.add(Integer.parseInt(reader.readLine()));
+
+        int p = 1;
+        int max = 1;
+        for (int j = 0; j < (list.size() - 1); j++){
+            if ((list.get(j).equals(list.get(j+1))) && (max <= p)){
+                p++;
+                max = p;
+            }
+            else if ((list.get(j).equals(list.get(j+1))) && (max > p))
+                p++;
+            else
+                p = 1;
+        }
+
+        System.out.println(max);
     }
 }
