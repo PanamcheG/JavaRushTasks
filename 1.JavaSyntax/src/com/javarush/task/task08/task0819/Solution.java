0b83d797da994b7329d77309cbe523a4c0e1eadd
PanamcheG <yashkovich94@mail.ru>
2020-04-20 20:58:24 +0300

    20.04 tasks

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0819/Solution.java
@@ -0,0 +1,37 @@
+package com.javarush.task.task08.task0819;
+
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/* 
+Set из котов
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Set<Cat> cats = createCats();
+
+        Iterator<Cat> iterator = cats.iterator();
+        if (iterator.hasNext()){
+            iterator.next();
+            iterator.remove();
+        }
+
+        printCats(cats);
+    }
+
+    public static Set<Cat> createCats() {
+        Set<Cat> set = new HashSet<>();
+        set.add(new Cat());
+        set.add(new Cat());
+        set.add(new Cat());
+        return set;
+    }
+
+    public static void printCats(Set<Cat> cats) {
+        cats.forEach(s -> System.out.println(s));
+    }
+
+   public static class Cat {}
+}
