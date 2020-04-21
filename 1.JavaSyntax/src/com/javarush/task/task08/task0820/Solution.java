b70af1b7431e3bec643ae9ff010e61e2d9ed271b
PanamcheG <yashkovich94@mail.ru>
2020-04-21 20:47:48 +0300

    21.04 tasks

---
+++ 1.JavaSyntax/src/com/javarush/task/task08/task0820/Solution.java
@@ -0,0 +1,64 @@
+package com.javarush.task.task08.task0820;
+
+import javax.swing.text.html.HTMLDocument;
+import java.util.HashSet;
+import java.util.Iterator;
+import java.util.Set;
+
+/* 
+Множество всех животных
+*/
+
+public class Solution {
+    public static void main(String[] args) {
+        Set<Cat> cats = createCats();
+        Set<Dog> dogs = createDogs();
+
+        Set<Object> pets = join(cats, dogs);
+        printPets(pets);
+
+        removeCats(pets, cats);
+        printPets(pets);
+    }
+
+    public static Set<Cat> createCats() {
+        Set<Cat> result = new HashSet<>();
+        result.add(new Cat());
+        result.add(new Cat());
+        result.add(new Cat());
+        result.add(new Cat());
+
+        return result;
+    }
+
+    public static Set<Dog> createDogs() {
+        Set<Dog> result = new HashSet<>();
+        result.add(new Dog());
+        result.add(new Dog());
+        result.add(new Dog());
+
+        return result;
+    }
+
+    public static Set<Object> join(Set<Cat> cats, Set<Dog> dogs) {
+        Set<Object> pets = new HashSet<>();
+        pets.addAll(cats);
+        pets.addAll(dogs);
+
+        return pets;
+    }
+
+    public static void removeCats(Set<Object> pets, Set<Cat> cats) {
+        pets.removeAll(cats);
+    }
+
+    public static void printPets(Set<Object> pets) {
+        Iterator<Object> iterator = pets.iterator();
+        while (iterator.hasNext())
+            System.out.println(iterator.next());
+    }
+
+    public static class Cat{}
+
+    public static class Dog{}
+}
